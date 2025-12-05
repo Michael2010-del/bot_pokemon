1ae1128a67ef2871b0753649b1858d9f41666749
GAWWA <mixzelenin@yandex.ru>
2025-12-05 21:58:09 +0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -3,74 +3,86 @@ import requests
 
 class Pokemon:
     pokemons = {}
-    
+    # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
+
         self.pokemon_trainer = pokemon_trainer   
-        self.pokemon_number = randint(1, 1000)
-        
-        
-        self._load_pokemon_data()
-        
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+
+        self.hp = randint(10,100)
+        self.power = randint(10,100)
+
         Pokemon.pokemons[pokemon_trainer] = self
-    
-    def _load_pokemon_data(self):
-        """Загружает все данные покемона за один запрос"""
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
         response = requests.get(url)
-        
         if response.status_code == 200:
             data = response.json()
-            
-            # Картинка
-            self.img = data['sprites']['other']['official-artwork']['front_default']
-            
-            # Имя
-            self.name = data['forms'][0]['name']
-            
-            # Типы (может быть несколько)
-            self.types = [type_info['type']['name'] for type_info in data['types']]
-            
-            # Рост и вес 
-            self.height = data["height"] / 10  
-            self.weight = data["weight"] / 10  
-            
-            
+            return (data['sprites']['other']['official-artwork']['front_default'])
         else:
-            # Значения по умолчанию при ошибке
-            self._set_default_values()
+            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+       
     
-    def _set_default_values(self):
-        """Устанавливает значения по умолчанию"""
-        self.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-        self.name = "Pikachu"
-        self.types = ["electric"]
-        self.height = 0.4  
-        self.weight = 6.0  
-        
-    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+
+    # Метод класса для получения информации
     def info(self):
-        types_str = ", ".join(self.types)
-        return f"""
-        🏷️ Имя: {self.name.capitalize()}
-        👤 Тренер: {self.pokemon_trainer}
-        ⚡ Тип(ы): {types_str}
-        📏 Рост: {self.height} м
-        ⚖️ Вес: {self.weight} кг
-        """
-    
+        return f"""Имя твоего покеомона: {self.name}
+                Здоровье: {self.hp}
+                Сила: {self.power}"""
+
+    # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
     
-    def get_type(self):
-        """Возвращает основной тип покемона"""
-        return self.types[0] if self.types else "Universal"
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
+
+class Wizard(Pokemon):
+    def attack(self, enemy):
+        return super().attack(enemy)
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
     
-    def get_all_types(self):
-        """Возвращает все типы покемона"""
-        return self.types
-    
-    def get_weight_height(self):
-        """Возвращает рост и вес"""
-        return {"height": self.height, "weight": self.weight}
-    
-    
+
+
+wizard = Wizard("username1")
+fighter = Fighter("username2")
+
+print(wizard.info())
+print("#"*10)
+print(fighter.info())
+print("#"*10)
+print(wizard.attack(fighter))
+print(fighter.attack(wizard))
